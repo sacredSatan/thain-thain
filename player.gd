ceceac7b96c38f4c3136d411625de05b290d9a57
sacredSatan <iamniharpatel@gmail.com>
2023-09-17 02:04:43 +0530

    player movement and sprite animation stuff

---
+++ player.gd
@@ -0,0 +1,79 @@
+extends Area2D
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.animation = "idle"
+	$AnimatedSprite2D.play()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+		if($AnimatedSprite2D.animation == "idle"):
+			if(velocity.y != 0):
+				$AnimatedSprite2D.animation = "up_transition" if velocity.y < 0 else "down_transition"
+
+			if(velocity.x != 0):
+				$AnimatedSprite2D.animation = "left_transition" if velocity.x < 0 else "right_transition"
+					
+			if(velocity.x != 0 and velocity.y != 0):
+				if(velocity.y < 0):
+					$AnimatedSprite2D.animation = "top_left_transition" if velocity.x < 0 else "top_right_transition"
+				if(velocity.y > 0):
+					$AnimatedSprite2D.animation = "bottom_left_transition" if velocity.x < 0 else "bottom_right_transition"
+		
+		# puke
+		var animationNameSplitArr = $AnimatedSprite2D.animation.split("_")
+		if(animationNameSplitArr.size() == 1 or (animationNameSplitArr.size() != 3 and $AnimatedSprite2D.animation.ends_with("transition"))):
+			if(velocity.x != 0 and velocity.y != 0):
+				if(velocity.y < 0):
+					$AnimatedSprite2D.animation = "top_left_transition" if velocity.x < 0 else "top_right_transition"
+				if(velocity.y > 0):
+					$AnimatedSprite2D.animation = "bottom_left_transition" if velocity.x < 0 else "bottom_right_transition"
+	else:
+		if($AnimatedSprite2D.animation == "up"):
+			$AnimatedSprite2D.animation = "up_transition_reverse"
+		elif($AnimatedSprite2D.animation == "down"):
+			$AnimatedSprite2D.animation = "down_transition_reverse"
+		elif($AnimatedSprite2D.animation == "right"):
+			$AnimatedSprite2D.animation = "right_transition_reverse"
+		elif($AnimatedSprite2D.animation == "left"):
+			$AnimatedSprite2D.animation = "left_transition_reverse"
+		elif($AnimatedSprite2D.animation == "top_left"):
+			$AnimatedSprite2D.animation = "top_left_transition_reverse"
+		elif($AnimatedSprite2D.animation == "top_right"):
+			$AnimatedSprite2D.animation = "top_right_transition_reverse"
+		elif($AnimatedSprite2D.animation == "bottom_left"):
+			$AnimatedSprite2D.animation = "bottom_left_transition_reverse"
+		elif($AnimatedSprite2D.animation == "bottom_right"):
+			$AnimatedSprite2D.animation = "bottom_right_transition_reverse"
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if($AnimatedSprite2D.animation.ends_with("transition")):
+		$AnimatedSprite2D.animation = $AnimatedSprite2D.animation.trim_suffix("_transition")
+		$AnimatedSprite2D.play();
+	
+	if($AnimatedSprite2D.animation.ends_with("reverse")):
+		$AnimatedSprite2D.animation = "idle";
+		$AnimatedSprite2D.play();
+
